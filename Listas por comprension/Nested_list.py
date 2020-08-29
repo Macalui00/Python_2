c09d1f1bfa721cc7fb884a894c8d2660f4127dfc
Macarena Lui <39487444+Macalui00@users.noreply.github.com>
2020-08-29 20:07:57 -0300

    Basta por hoy

--- Listas por comprension/Nested_list.py
+++ Listas por comprension/Nested_list.py
@@ -9,3 +9,8 @@ print(listaAnidada[1][-1])
 for l in listaAnidada:
     for val in l:
         print(val)
+
+board = [[num for num in range(1,4)] for valr in range(1,4)]
+print(board)
+
+print([["x" if num % 2 != 0 else "0" for num in range (1,4)] for val in range (1,4)])
